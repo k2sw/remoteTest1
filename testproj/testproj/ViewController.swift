8ad40292514b62a1a027576eb7f8b4e30ac3fdfc
Hiroshi Gotoh <hiroshi.cd.gotoh@jp.ricoh.com>
2019-06-14 16:59:45 +0900

    1st

---
+++ testproj/testproj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testproj
+//
+//  Created by Hiroshi Gotoh on 2019/06/14.
+//  Copyright © 2019 Hiroshi Gotoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
